20a4a078802dc63d8d62ffc025d4b977a32e1341
Asqar <asqar@mail.ru>
2022-10-18 10:30:13 +0400

    Displaying Products

--- index.php
+++ index.php
@@ -78,20 +78,37 @@ include ('includes/connect.php');
 
         </div> <!--third child ends-->
 
-        <div class="row"> <!--fourth child starts-->
+        <div class="row mx-2 p-1"> <!--fourth child starts-->
             <div class="col-md-10">
-                <div class="row"> <!--products starts-->
-                    <div class="col-md-4 mb-2">
-                        <div class="card" >
-                            <img src="./images/apple.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-info">Add to cart</a>
-                                <a href="#" class="btn btn-secondary">View more</a>
+                <div class='row'> <!--products starts-->
+                <?php
+                $select_query = "select * from `products` order by product_title LIMIT 0,9";
+                $result_query = mysqli_query($con,$select_query);
+
+                while ($row = mysqli_fetch_assoc($result_query)){
+                    $product_id = $row['product_id'];
+                    $product_title = $row['product_title'];
+                    $product_description = $row['product_description'];
+                    $category_id = $row['category_id'];
+                    $brand_id = $row['brand_id'];
+                    $product_image1 = $row['product_image1'];
+                    $product_price = $row['product_price'];
+
+                    echo "
+                    
+                    <div class='col-md-4 mb-2'>
+                        <div class='card' >
+                            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
+                            <div class='card-body'>
+                                <h5 class='card-title'>$product_title</h5>
+                                <p class='card-text'>$product_description</p>
+                                <a href='#' class='btn btn-info'>Add to cart</a>
+                                <a href='#' class='btn btn-secondary'>View more</a>
                             </div>
                         </div>
-                    </div>
+                    </div>";
+                }
+                ?>
                 </div> <!--products ends-->
             </div>
             <div class="col-md-2 bg-secondary p-0"> <!--sidenav starts-->
