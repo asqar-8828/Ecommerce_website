d3a047a393534775ace860aefe641614a64c9481
Asqar <asqar@mail.ru>
2022-10-20 14:16:48 +0400

    Doing changes in Cart table

--- functions/common_function.php
+++ functions/common_function.php
@@ -29,6 +29,7 @@ function getproducts () {
                             <div class='card-body'>
                                 <h5 class='card-title'>$product_title</h5>
                                 <p class='card-text'>$product_description</p>
+                                <p class='card-text'>Price: $product_price</p>
                                 <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>
                                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
                             </div>
@@ -68,6 +69,7 @@ function get_all_products(){
                             <div class='card-body'>
                                 <h5 class='card-title'>$product_title</h5>
                                 <p class='card-text'>$product_description</p>
+                                <p class='card-text'>Price: $product_price</p>
                                 <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>
                                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
                             </div>
@@ -108,6 +110,7 @@ function get_unique_categories() {
                             <div class='card-body'>
                                 <h5 class='card-title'>$product_title</h5>
                                 <p class='card-text'>$product_description</p>
+                                <p class='card-text'>Price: $product_price</p>
                                 <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>
                                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
                             </div>
@@ -149,6 +152,7 @@ function get_unique_brands() {
                             <div class='card-body'>
                                 <h5 class='card-title'>$product_title</h5>
                                 <p class='card-text'>$product_description</p>
+                                <p class='card-text'>Price: $product_price</p>
                                 <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>
                                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
                             </div>
@@ -222,6 +226,7 @@ function search_product(){
                             <div class='card-body'>
                                 <h5 class='card-title'>$product_title</h5>
                                 <p class='card-text'>$product_description</p>
+                                <p class='card-text'>Price: $product_price</p>
                                 <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>
                                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
                             </div>
@@ -264,6 +269,7 @@ function view_details() {
                             <div class='card-body'>
                                 <h5 class='card-title'>$product_title</h5>
                                 <p class='card-text'>$product_description</p>
+                                <p class='card-text'>Price: $product_price</p>
                                 <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>
                                 <a href='index.php' class='btn btn-secondary'>Go home</a>
                             </div>
