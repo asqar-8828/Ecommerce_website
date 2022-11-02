ae60803e4268fa4b4a9c1c160a1980b258e07a54
Asqar <asqar@mail.ru>
2022-11-02 14:31:15 +0400

    Displaying empty cart message

--- cart.php
+++ cart.php
@@ -81,17 +81,7 @@ include ('./functions/common_function.php');
         <div class="row">
             <form action="" method="post">
             <table class="table table-bordered text-center">
-                <thead>
-                    <tr>
-                        <th>Product title</th>
-                        <th>Product image</th>
-                        <th>Quantity</th>
-                        <th>Total price</th>
-                        <th>Remove</th>
-                        <th colspan="2">Operations</th>
-                    </tr>
-                </thead>
-                <tbody>
+
                     <!-- php code to display dynamic data -->
                     <?php
 
@@ -99,6 +89,21 @@ include ('./functions/common_function.php');
                     $get_ip_add = getIPAddress();
                     $cart_query = "select * from `cart_details` where ip_address = '$get_ip_add'";
                     $result = mysqli_query($con,$cart_query);
+                    $result_query = mysqli_num_rows($result);
+                    if ($result_query > 0) {
+                        echo "
+                        <thead>
+                    <tr>
+                        <th>Product title</th>
+                        <th>Product image</th>
+                        <th>Quantity</th>
+                        <th>Total price</th>
+                        <th>Remove</th>
+                        <th colspan='2'>Operations</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        ";
                     while ($row=mysqli_fetch_array($result)) {
                         $product_id = $row['product_id'];
                         $select_products = "select * from `products` where product_id='$product_id' ";
@@ -138,15 +143,38 @@ include ('./functions/common_function.php');
                         <?php
                                 }
                                     }
-                        ?>
+                    }
+                    else {
+                        echo "<h2 class='text-center text-danger'>Cart is empty </h2>";
+                    }
+                    ?>
                 </tbody>
 
             </table>
             <!--Subtotal-->
             <div class="d-flex mb-5">
-                <h4 class="px-3">Subtotal: <strong class="text-info"><?php echo $total_price; ?></strong></h4>
-                <a href="index.php"><button class="bg-info px-3 py-2 mx-3 border-0">Continue Shopping</button></a>
-                <a href="#"><button class="bg-secondary px-3 py-2 border-0 text-light">Checkout</button></a>
+                <?php
+
+                $get_ip_add = getIPAddress();
+                $cart_query = "select * from `cart_details` where ip_address = '$get_ip_add'";
+                $result = mysqli_query($con,$cart_query);
+                $result_query = mysqli_num_rows($result);
+                if ($result_query > 0) {
+                    echo "
+                    <h4 class='px-3'>Subtotal: <strong class='text-info'> $total_price </strong></h4>
+                    <input type='submit' value='Continue Shopping' class='bg-info px-3 py-2 mx-3 border-0' name='continue_shopping'>
+                    <a href='#'><button class='bg-secondary px-3 py-2 border-0 text-light'>Checkout</button></a>    
+                    ";
+                } else {
+                    echo "
+                    <input type='submit' value='Continue Shopping' class='bg-info px-3 py-2 mx-3 border-0' name='continue_shopping'>
+                    ";
+                }
+                if (isset($_POST['continue_shopping'])){
+                    echo "<script>window.open('index.php','_self')</script>";
+                }
+                ?>
+
 
             </div>
 
